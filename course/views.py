caa0b8c25fafca191de6854344db3556af771c10
Renttor <denis30510@gmail.com>
2023-10-04 13:11:34 +0300

    fixed permission and queryset

--- course/views.py
+++ course/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from course.models import Course, Lesson, Payments
-from course.permissions import IsStaffViewSet, IsStaff, IsOwner
+from course.permissions import IsStaffViewSet, IsStaff, IsUser
 from course.seriliazers import CourseSerializer, LessonSerializer, PaymentsSerializer
 from course.service import CreateMixin
 from rest_framework.response import Response
@@ -28,7 +28,7 @@ class CourseViewSet(CreateMixin, viewsets.ModelViewSet):
 
 class LessonCreateAPIView(CreateMixin, generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsUser | IsAdminUser]
 
 
 class LessonListAPIView(generics.ListAPIView):
@@ -39,26 +39,29 @@ class LessonListAPIView(generics.ListAPIView):
     def get_queryset(self):
         if self.request.user.group == 'user':
             queryset = Lesson.objects.filter(owner=self.request.user)
+            print(queryset)
         elif self.request.user.group == 'staff':
             queryset = Lesson.objects.all()
+        else:
+            queryset = Lesson.objects.none()
         return queryset
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner | IsStaff]
+    permission_classes = [IsAuthenticated, IsUser | IsStaff]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner | IsStaff]
+    permission_classes = [IsAuthenticated, IsUser | IsStaff]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsUser]
 
 
 class PaymentsListAPIView(generics.ListAPIView):
