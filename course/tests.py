cc9cc5b2327f804a26d15771b73eb2da6c831b30
Renttor <denis30510@gmail.com>
2023-10-06 14:54:00 +0300

    test for create lesson

--- course/tests.py
+++ course/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = User.objects.create(email='user@test.com', password='test', group='user')
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_lesson(self):
+        """ Тестирование создание урока """
+        data = {
+            "title": 'test',
+            'description': 'test',
+            "video_url": "http://youtube.com"
+        }
+        response = self.client.post(
+            '/lesson/create/',
+            data=data
+        )
+
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
